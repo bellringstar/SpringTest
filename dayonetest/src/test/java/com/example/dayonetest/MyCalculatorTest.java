9d8839ffd568c6a84416c6560e7218f4f3c1eb2e
김현종 <dw3510@naver.com>
2023-10-20 15:55:16 +0900

    pass

---
+++ dayonetest/src/test/java/com/example/dayonetest/MyCalculatorTest.java
@@ -0,0 +1,25 @@
+package com.example.dayonetest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+class MyCalculatorTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void minus() {
+    }
+
+    @Test
+    void multiply() {
+    }
+
+    @Test
+    void divide() {
+    }
+}
