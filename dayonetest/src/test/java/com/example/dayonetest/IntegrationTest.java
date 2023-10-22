13b9ac17773cf2e2f72f8baca1d13a34a652f087
bellring <starringbell@gmail.com>
2023-10-22 22:42:30 +0900

    redis integrationTest 작성

--- dayonetest/src/test/java/com/example/dayonetest/IntegrationTest.java
+++ dayonetest/src/test/java/com/example/dayonetest/IntegrationTest.java
@@ -1,5 +1,6 @@
 package com.example.dayonetest;
 
+import com.redis.testcontainers.RedisContainer;
 import jakarta.transaction.Transactional;
 import java.io.File;
 import java.time.Duration;
@@ -21,6 +22,7 @@ import org.testcontainers.containers.wait.strategy.Wait;
 public class IntegrationTest {
 
     static DockerComposeContainer rdbms;
+    static RedisContainer redis;
 
     static {
         rdbms = new DockerComposeContainer(new File("infra/test/docker-compose.yaml"))
@@ -36,6 +38,9 @@ public class IntegrationTest {
                                 .withStartupTimeout(Duration.ofSeconds(300))
                 );
         rdbms.start();
+
+        redis = new RedisContainer(RedisContainer.DEFAULT_IMAGE_NAME.withTag("6"));
+        redis.start();
     }
 
     static class IntegrationTestInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
@@ -47,6 +52,12 @@ public class IntegrationTest {
             var rdbmsPort = rdbms.getServicePort("local-db", 3306);
 
             properties.put("spring.datasource.url", "jdbc:mysql://" + rdbmsHost + ":" + rdbmsPort + "/score");
+
+            var redisHost = redis.getHost();
+            var redisPort = redis.getFirstMappedPort();
+            properties.put("spring.data.redis.host", redisHost);
+            properties.put("spring.data.redis.port", redisPort.toString());
+
 
             TestPropertyValues.of(properties)
                     .applyTo(applicationContext);
